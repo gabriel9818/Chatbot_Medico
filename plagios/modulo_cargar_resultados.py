6e56d48255b55ad932acda84507d41e148b34eef
gabriel9818 <113267193+gabriel9818@users.noreply.github.com>
2022-09-10 23:54:17 -0500

    first chatbot

--- plagios/modulo_cargar_resultados.py
+++
@@ -1,7 +0,0 @@
-#modulo cargar_resultados
-"""
-modulo que carga el etiquetador gramatical generado previamente con tagger_init.py
-"""
-from pickle import load as pklLoad
-carga = open('results.pkl', 'rb')
-resultados = pklLoad(carga)
--- plagios/modulo_cargar_tagger.py
+++
@@ -1,7 +0,0 @@
-#modulo cargar_tagger
-"""
-modulo que carga el etiquetador gramatical generado previamente con tagger_init.py
-"""
-from pickle import load as pklLoad
-carga = open('wiki_tagger_u.pkl', 'rb')
-tagger = pklLoad(carga)
