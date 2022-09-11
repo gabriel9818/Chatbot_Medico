e73630dee8dfc34eb4e6b58801dc626157786398
gabriel9818 <113267193+gabriel9818@users.noreply.github.com>
2022-09-10 23:45:04 -0500

    first commit

---
+++ plagios/modulo_cargar_resultados.py
@@ -0,0 +1,7 @@
+#modulo cargar_resultados
+"""
+modulo que carga el etiquetador gramatical generado previamente con tagger_init.py
+"""
+from pickle import load as pklLoad
+carga = open('results.pkl', 'rb')
+resultados = pklLoad(carga)
